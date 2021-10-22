94bee588aa1f88e5a794f6bbcad7671c6960ff03
itigo-11111 <38446219+itigo-11111@users.noreply.github.com>
2021-10-22 17:53:17 +0900

    Update architect.py

--- architect.py
+++ architect.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import sort
 import torch
 import numpy as np
 import torch.nn as nn
@@ -5,6 +6,8 @@ from torch.autograd import Variable
 # from decimal import Decimal, ROUND_DOWN
 from decimal import Decimal, ROUND_HALF_UP
 import logging
+import random
+from model import NetworkCIFAR as Network2
 
 def _concat(xs):
   return torch.cat([x.view(-1) for x in xs])
@@ -19,8 +22,8 @@ class Architect(object):
     self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
         lr=args.arch_learning_rate, betas=(0.5, 0.999), weight_decay=args.arch_weight_decay)
     self.optimizer_gammas = torch.optim.SGD(self.model.arch_gammas_parameters(),
-        lr=args.gammas_learning_rate, weight_decay=False)
-
+        lr=args.gammas_learning_rate, weight_decay=args.arch_weight_decay)
+    self.auxiliary = args.auxiliary
     self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
             self.optimizer_gammas, float(args.epochs), eta_min=args.learning_rate_min)
 
@@ -48,15 +51,58 @@ class Architect(object):
     # print(self.model.alphas_normal)
     # print(self.model.gammas_normal)
 
-  def param_step(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, pytorch_total_params_train,step,limit_param, num_flag,lambda_a):
+  def param_step(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, pytorch_total_params_train,step,limit_param, num_flag,lambda_a,param_prev):
 
+    genotype = self.model.genotype()
+    train_param = pytorch_total_params_train
+    # if param_prev == pytorch_total_params_train: 
+     
 
     self.model.gammas_normal.data = self.model.alphas_normal[:,4:].clone().detach()
     self.model.gammas_reduce.data = self.model.alphas_reduce[:,4:].clone().detach()
 
     aa = self.model.gammas_normal.clone().detach()
-    self.optimizer_gammas.zero_grad()
+
+    
     # self.model.gammas_normal =  torch.tensor(self.model.gammas_normal + 4.0, requires_grad=True)
+    # DropArchWeight
+    if train_param == param_prev: 
+      with torch.no_grad():
+        # normal cell
+        i = 0
+        while True:    
+          n = random.randint(0,13)
+          num = torch.argmax(self.model.alphas_normal, dim=1)[n]
+          if num > 3 or i > 20:
+            break
+          i += 1
+        # self.model.alphas_normal[n][num] = torch.max(self.model.alphas_normal[n,:].clone().detach())
+        sorted , _ = torch.sort(self.model.alphas_normal[n,:].clone().detach(),descending=True)
+        self.model.alphas_normal[n][num] = sorted[2]
+
+        # print("n:{} num:{}".format(n,num))
+
+        # reduce cell
+        i = 0
+        while True:    
+          n = random.randint(0,13)
+          num = torch.argmax(self.model.alphas_reduce, dim=1)[n]
+          if num > 3 or i > 20:
+            break
+          i += 1
+        # self.model.alphas_reduce[n][num] = torch.min(self.model.alphas_reduce[n,:].clone().detach())  
+        sorted , _ = torch.sort(self.model.alphas_reduce[n,:].clone().detach(),descending=True)
+        self.model.alphas_reduce[n][num] = sorted[2]
+
+      genotype = self.model.genotype()
+      CIFAR_CLASSES = 10
+      model3 = Network2(36, CIFAR_CLASSES, 20, self.auxiliary, genotype)
+      train_param = sum(p.numel() for p in model3.parameters() if p.requires_grad)
+      pytorch_total_params_train = train_param
+      self.model.gammas_normal.data = self.model.alphas_normal[:,4:].clone().detach()
+      self.model.gammas_reduce.data = self.model.alphas_reduce[:,4:].clone().detach()
+
+    self.optimizer_gammas.zero_grad()
 
     # np.set_printoptions(suppress=True)
     np.set_printoptions(precision=4)
@@ -70,26 +116,26 @@ class Architect(object):
 
 
     # print("param:{} limit:{}".format(pytorch_total_params_train,limit_param)) 
-    # if pytorch_total_params_train > limit_param :
-    self.param_backward_step(input_valid, target_valid, pytorch_total_params_train,step,limit_param,num_flag,lambda_a)
+    if pytorch_total_params_train > limit_param :
+      self.param_backward_step(input_valid, target_valid, pytorch_total_params_train,step,limit_param,num_flag,lambda_a)
     
     
-    self.optimizer_gammas.step()
-    self.scheduler.step()
-    # print(self.model.alphas_normal)
-    # print(self.model.betas_normal)
-    # print(self.model.gammas_normal)
-    bb = self.model.gammas_normal.clone().detach()
-    logging.info(aa - bb)
-    # print(aa - bb)
-    # print(self.model.alphas_reduce)
+      self.optimizer_gammas.step()
+      self.scheduler.step()
+      # print(self.model.alphas_normal)
+      # print(self.model.betas_normal)
+      # print(self.model.gammas_normal)
+      bb = self.model.gammas_normal.clone().detach()
+      logging.info(aa - bb)
+      # print(aa - bb)
+      # print(self.model.alphas_reduce)
 
 
-    gammas_normal_nonparam = self.model.alphas_normal[:,:4].clone().detach()
-    gammas_reduce_nonparam = self.model.alphas_reduce[:,:4].clone().detach()
-        
-    self.model.alphas_normal.data = torch.cat([gammas_normal_nonparam,self.model.gammas_normal],dim=1)
-    self.model.alphas_reduce.data = torch.cat([gammas_reduce_nonparam,self.model.gammas_reduce],dim=1)
+      gammas_normal_nonparam = self.model.alphas_normal[:,:4].clone().detach()
+      gammas_reduce_nonparam = self.model.alphas_reduce[:,:4].clone().detach()
+          
+      self.model.alphas_normal.data = torch.cat([gammas_normal_nonparam,self.model.gammas_normal],dim=1)
+      self.model.alphas_reduce.data = torch.cat([gammas_reduce_nonparam,self.model.gammas_reduce],dim=1)
     # print("after:")
     # print(self.model.alphas_normal)
     # print(self.model.betas_normal)
@@ -98,6 +144,7 @@ class Architect(object):
   # def _backward_step_old(self, input_valid, target_valid,pytorch_total_params_train,step,limit_param):
   #   loss = self.model._loss(input_valid, target_valid)
   #   loss.backward()
+    return train_param,genotype
 
   def _backward_step(self, input_valid, target_valid,pytorch_total_params_train,step,limit_param,num_flag):
     loss = self.model._loss(input_valid, target_valid,num_flag)
@@ -121,10 +168,12 @@ class Architect(object):
     # print(input_valid.grad)
     # print(Decimal(self.model.gammas_normal.grad).quantize(Decimal(.00000000’),rounding=ROUND_HALF_UP))
     # print("{0:.7f}".format(Decimal(self.model.gammas_normal.grad)))
+    
     self.model.gammas_normal.grad = abs(self.model.gammas_normal.grad)
     self.model.gammas_reduce.grad = abs(self.model.gammas_reduce.grad)
     self.model.betas_normal.grad = abs(self.model.betas_normal.grad)
     self.model.betas_reduce.grad = abs(self.model.betas_normal.grad)
+
     # print(self.model.gammas_normal.grad)
 
   def _backward_step_unrolled(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer,device):
@@ -176,4 +225,3 @@ class Architect(object):
       p.data.add_(R, v)
 
     return [(x-y).div_(2*R) for x, y in zip(grads_p, grads_n)]
-
